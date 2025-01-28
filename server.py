724ab85e3aa65139e3000b54333d47310883803e
rihtrazh <132520216+rihtrazh@users.noreply.github.com>
2025-01-29 03:47:12 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,31 @@
 import socket
+import threading
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(0)
-conn, addr = sock.accept()
-print(addr)
+def handle_client(client_socket, client_address):
+    print(f"Новый клиент {client_address} подключен")
+    while True:
+        try:
+            message = client_socket.recv(1024)
+            if not message:
+                break
+            print(f"Получено сообщение от {client_address} {message.decode()}")
+            client_socket.send(message)
+        except ConnectionResetError:
+            break
+    print(f"Клиент отключен {client_address}")
+    client_socket.close()
 
-msg = ''
+def start_server():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 5555))
+    server_socket.listen(5)
+    print("Сервер запущен / oжидание подключения клиентов")
 
-while True:
-	data = conn.recv(1024)
-	if not data:
-		break
-	msg += data.decode()
-	conn.send(data)
+    while True:
+        client_socket, client_address = server_socket.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+        client_thread.start()
+        print(f"Активные потоки {threading.active_count() - 1}")
 
-print(msg)
-
-conn.close()
+if __name__ == "__main__":
+    start_server()
